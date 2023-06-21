d5f3f81f6eff3331f95aa9c67bfcc60277cea5a9
huanminh1612 <huanvo2001@gmail.com>
2023-06-21 12:33:37 +0700

    Update home page

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -11,7 +11,7 @@
         rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" 
         integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
-    <title>Sixteen Clothing Template</title>
+    <title>Henrik Clothing</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
